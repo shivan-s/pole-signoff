b7e30c602e0ab758e9483e827fb1817bf00577be
Shivan Sivakumaran <me@shivan.xyz>
2024-09-20 22:30:35 +1200

    chore: formatting

--- src/lib/components/Spinner.svelte
+++ src/lib/components/Spinner.svelte
@@ -1,25 +1,26 @@
 <script lang="ts">
-
 </script>
-<span class="loader" />
-<style>
- span.loader {
-    width: 48px;
-    height: 48px;
-    border: 5px solid #FFF;
-    border-bottom-color: var(--primary);
-    border-radius: 50%;
-    display: inline-block;
-    box-sizing: border-box;
-    animation: rotation 1s linear infinite;
-    }
 
-    @keyframes rotation {
-    0% {
-        transform: rotate(0deg);
-    }
-    100% {
-        transform: rotate(360deg);
-    }
-    }    
+<span class="loader" />
+
+<style>
+	span.loader {
+		width: 48px;
+		height: 48px;
+		border: 5px solid #fff;
+		border-bottom-color: var(--primary);
+		border-radius: 50%;
+		display: inline-block;
+		box-sizing: border-box;
+		animation: rotation 1s linear infinite;
+	}
+
+	@keyframes rotation {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
+	}
 </style>
