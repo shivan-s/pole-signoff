51a86713278cf19967e413d7cb962fec230fff63
Shivan Sivakumaran <me@shivan.xyz>
2024-09-22 10:26:32 +1200

    chore: fix imports

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,5 +3,5 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
 console.log('database', DATABASE_URL);
-const client = postgres(DATABASE_URL);
+const client = postgres(DATABASE_URL, { prepare: false });
 export const db = drizzle(client);
