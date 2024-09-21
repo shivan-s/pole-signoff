ecf0c4afd79614a1a7c0cf50f28f8056d3490f60
Shivan Sivakumaran <me@shivan.xyz>
2024-09-21 23:16:48 +1200

    chore: update db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'drizzle-kit';
 config({ path: '.env' });
 
 export default defineConfig({
-	schema: './src/lib/db/schema.ts',
+	schema: './src/lib/server/db/schema.ts',
 	out: './supabase/migrations',
 	dialect: 'postgresql',
 	dbCredentials: {
