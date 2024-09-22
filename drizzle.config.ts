092a9a3e10985e1e396853138eb3b9e3dbf1d2ae
Shivan Sivakumaran <me@shivan.xyz>
2024-09-22 14:13:57 +1200

    chore: switch to sqlite

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
-	out: './drizzle',
+	out: './src/lib/server/db/migrations',
 	dialect: 'sqlite',
 	driver: 'd1-http',
 	dbCredentials: {
