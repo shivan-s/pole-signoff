78760bb9f1355f3f5d7fcbcd2049f2a4a2353932
Shivan Sivakumaran <me@shivan.xyz>
2024-11-14 17:07:54 +1300

    chore: add table for invite codes

--- src/routes/invite/+page.server.ts
+++ src/routes/invite/+page.server.ts
@@ -1,7 +1,10 @@
+import { fetchInviteCodesFromUser } from '$lib/server/db/inviteCodes';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
+	const inviteCodes = await fetchInviteCodesFromUser(locals.user.id);
 	return {
+		inviteCodes,
 		user: locals.user,
 		pageTitle: 'Invite Codes'
 	};
